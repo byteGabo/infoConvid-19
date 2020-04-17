c5b2a4660c6c8dd484eb9f7710ed47bad592187a
Gabriel Chaves <gabovvv@gmail.com>
2020-04-16 19:59:34 -0600

    recycleview all countries

--- app/src/main/java/com/gabo/infoconvid_19/GlobalFragment.java
+++ app/src/main/java/com/gabo/infoconvid_19/GlobalFragment.java
@@ -3,16 +3,40 @@ package com.gabo.infoconvid_19;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class GlobalFragment extends Fragment {
+
+    RecyclerView rvCovidCountry;
+    ProgressBar progressBar;
+
+
+    private static final String TAG = GlobalFragment.class.getSimpleName();
+    ArrayList<CovidCountry> covidCountries;
+
 
     public GlobalFragment() {
         // Required empty public constructor
@@ -23,6 +47,56 @@ public class GlobalFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_global, container, false);
+        View root  = inflater.inflate(R.layout.fragment_global, container, false);
+
+        rvCovidCountry = root.findViewById(R.id.rvCovidCountry);
+        progressBar = root.findViewById(R.id.progress_circular_country);
+        rvCovidCountry.setLayoutManager(new LinearLayoutManager(getActivity()));
+        
+        //call volley method
+        getDataFromServer();
+
+        return root;
+    }
+
+    private void showRecyclerView(){
+        CovidCountryAdapter covidCountryAdapter = new CovidCountryAdapter(covidCountries);
+        rvCovidCountry.setAdapter(covidCountryAdapter);
+    }
+
+
+    private void getDataFromServer() {
+        String url = "https://corona.lmao.ninja/v2/countries";
+        covidCountries = new ArrayList<>();
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                progressBar.setVisibility(View.GONE);
+                if (response!=null){
+                    Log.e(TAG, "onResponse:" + response);
+                    try {
+                        JSONArray jsonArray = new JSONArray(response);
+                        for (int i = 0; i < jsonArray.length(); i++){
+                            JSONObject data = jsonArray.getJSONObject(i);
+                            covidCountries.add(new CovidCountry(data.getString("country"), data.getString("cases")));
+                        }
+                        showRecyclerView();
+                    } catch (JSONException e){
+                        e.printStackTrace();
+                    }
+                }
+
+
+            }
+
+                },new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                progressBar.setVisibility(View.GONE);
+                Log.e(TAG, "onResponse"+error);
+            }
+        });
+        Volley.newRequestQueue(getActivity()).add(stringRequest);
     }
 }
